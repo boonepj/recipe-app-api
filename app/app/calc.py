4e7e0607d1432cffd8ea2cb2680f02b8eab41b8e
Paul Boone <paul.boone@gmail.com>
2023-04-26 11:23:59 -0400

    section 37

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+
+"""
+
+def add(x,y) : 
+    """ Add x & y retrun result """
+    return x + y
+
+
+def subtract(x,y) : 
+    """ sub x & y retrun result """
+    return x - y
+    
---
+++ app/app/tests.py
@@ -0,0 +1,26 @@
+"""
+
+Sample tests
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module """
+
+    def test_add_numbers(self):
+        """ test adding numbers """
+        res = calc.add(6,6)
+
+        self.assertEqual(res, 12)
+
+
+    def test_substract_numbers(self):
+        """ test adding numbers """
+        res = calc.subtract(6,6)
+
+        self.assertEqual(res, 0)
